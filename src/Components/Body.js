26027fd345f0385007198b87580d377283054d72
tonyykosseifyy <tonykosseify123@gmail.com>
2021-07-30 16:31:35 +0300

    Creating the Container Component

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,11 +1,18 @@
 import React from 'react' ;
 import '../styles/Body.css' ;
+import { data } from '../data.js' ;
+import Container from './Container' ;
 
 
 const Body = () => {
   return (
     <div className='body'>
-    
+      { data.map((item , index ) => (
+        <Container key={index}>
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+        </Container>
+      ))}
     </div>
   ) ;
 } ;
---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const data = [
+  {
+    title: 'Supervisor' ,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta quasi temporibus asperiores vel, a qui.' ,  
+  } , 
+  {
+    title: 'Team Biulder' ,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta quasi temporibus asperiores vel, a qui.' , 
+  }, 
+  {
+    title: 'Karma' ,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta quasi temporibus asperiores vel, a qui.' , 
+  }, 
+  {
+    title: 'Calculator' ,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta quasi temporibus asperiores vel, a qui.' , 
+  }
+] ;
+
