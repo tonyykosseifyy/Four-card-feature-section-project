6a88db7369db241f609a75f83806351d9057ca11
tonyykosseifyy <tonykosseify123@gmail.com>
2021-07-30 14:20:48 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-heeynz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-heeynz)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Global.css' ;
+import './styles/App.css' ;
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+} ;
+
+
+
+export default App ;
