b0cbd0426012a0b3913ef843efaac3ff0cd71e56
tonyykosseifyy <tonykosseify123@gmail.com>
2021-07-30 16:00:05 +0300

    Creating the Body Component

---
+++ src/Components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react' ;
+import '../styles/Body.css' ;
+
+
+const Body = () => {
+  reutrn (
+    <div className='body'>
+    
+    </div>
+  ) ;
+} ;
+
+
+export default Body ;
